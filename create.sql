4e43e31693bd9bb282f9f4db7fc47673cb008276
Chris Yu <jacky79310@gmail.com>
2020-11-08 18:30:04 +0800

    modifed sql

--- create.sql
+++ create.sql
@@ -3,7 +3,7 @@ create database short_url;
 
 /*drop table url_mapping;*/
 create table url_mapping(
-   idx BIGINT(8) UNSIGNED NOT NULL,
+   idx BIGINT(8) UNSIGNED NOT NULL, 
    short_url varchar(255) NOT NULL,
    origin_url varchar(2048) NOT NULL,
    unique(short_url),
